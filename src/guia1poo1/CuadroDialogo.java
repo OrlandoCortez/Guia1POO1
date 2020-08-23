16c1b0a41770150face7c1d2345b398fe940a5e6
OrlandoCortez <201702193@ujmd.edu.sv>
2020-08-23 17:46:02 -0600

    Guia 1 POO

---
+++ src/guia1poo1/CuadroDialogo.java
@@ -0,0 +1,12 @@
+package guia1poo1;
+import javax.swing.JOptionPane;
+public class CuadroDialogo {
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+// TODO code application logic here
+JOptionPane.showMessageDialog(null, "Bienvenido a NetBeans IDE 8.2");
+System.exit(0);
+}
+}
